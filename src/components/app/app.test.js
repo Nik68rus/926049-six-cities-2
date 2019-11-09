71599da005c19111bd3975b14b301778a0d3c128
Никита Ларичкин <nikita@MacBook-Pro-Nikita.local>
2019-11-09 21:55:57 +0300

    доработал тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,11 +1,9 @@
 import renderer from 'react-test-renderer';
 import App from '../app/app';
 
-it(`App correctly renders after relaunch`, () => {
-  const div = document.createElement(`div`);
-  div.id = `map`;
-  document.body.appendChild(div);
+jest.mock(`../map/map`);
 
+it(`App correctly renders after relaunch`, () => {
   const tree = renderer
     .create(<App
       offers={[]}
--- src/components/main-screen/main-screen.test.js
+++ src/components/main-screen/main-screen.test.js
@@ -1,10 +1,9 @@
 import renderer from 'react-test-renderer';
 import MainScreen from '../main-screen/main-screen';
 
+jest.mock(`../map/map`);
+
 it(`Main screen correctly renders after relaunch`, () => {
-  const div = document.createElement(`div`);
-  div.id = `map`;
-  document.body.appendChild(div);
   const tree = renderer
     .create(<MainScreen
       offers={[]}
