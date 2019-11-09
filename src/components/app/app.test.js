05af85bd200e610af73adc2b3c4072fb22dc0536
Nik68rus <nik200x@mail.ru>
2019-11-09 16:13:46 +0300

    доработал тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,6 +2,10 @@ import renderer from 'react-test-renderer';
 import App from '../app/app';
 
 it(`App correctly renders after relaunch`, () => {
+  const div = document.createElement(`div`);
+  div.id = `map`;
+  document.body.appendChild(div);
+
   const tree = renderer
     .create(<App
       offers={[]}
--- src/components/main-screen/main-screen.test.js
+++ src/components/main-screen/main-screen.test.js
@@ -2,6 +2,9 @@ import renderer from 'react-test-renderer';
 import MainScreen from '../main-screen/main-screen';
 
 it(`Main screen correctly renders after relaunch`, () => {
+  const div = document.createElement(`div`);
+  div.id = `map`;
+  document.body.appendChild(div);
   const tree = renderer
     .create(<MainScreen
       offers={[]}
